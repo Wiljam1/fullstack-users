14a2bf885fccf7a3d13d28af674329a2a3f99d22
Wiljam Wilmi <wiljam.wilmi@hotmail.com>
2023-12-11 22:15:38 +0100

    logging test

--- src/main/java/kth/wiljam/fullstackusers/FullstackUsersApplication.java
+++ src/main/java/kth/wiljam/fullstackusers/FullstackUsersApplication.java
@@ -1,13 +1,20 @@
 package kth.wiljam.fullstackusers;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class FullstackUsersApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(FullstackUsersApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(FullstackUsersApplication.class, args);
+        logger.info("Logging test, application started");
     }
 
 }
