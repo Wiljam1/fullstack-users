99f222533edca9b1a171abab19a364913002dbb9
Niklas Roslund <nroslund@kth.se>
2023-12-30 08:02:35 +0100

    fix

--- src/main/java/kth/wiljam/fullstackusers/KeyCloak/SecurityConfig.java
+++ src/main/java/kth/wiljam/fullstackusers/KeyCloak/SecurityConfig.java
@@ -1,26 +1,36 @@
 package kth.wiljam.fullstackusers.KeyCloak;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class SecurityConfig {
 
+    //@Autowired
+    //JwtAuthenticationConverter jwtAuthConverter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(t -> t.disable());
         http.authorizeHttpRequests(authorize -> {
-            authorize.anyRequest().authenticated();
+            authorize
+                    //.requestMatchers(HttpMethod.GET, "/resturant/public/list").permitAll()    //skriv om
+                    .anyRequest().authenticated();
         });
         http.oauth2ResourceServer(t -> {
             t.jwt(Customizer.withDefaults());
+            //t.opaqueToken(Customizer.withDefaults());
+            //configurer -> configurer.jwtAuthenticationConverter(jwtAuthConverter
         });
         http.sessionManagement(
                 t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
