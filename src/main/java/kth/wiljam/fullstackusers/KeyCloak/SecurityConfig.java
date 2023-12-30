bdbb55dd5bd93aa9f12c977209eaa24287bd506b
Niklas Roslund <nroslund@kth.se>
2023-12-30 01:24:47 +0100

    inital keycloak security

--- src/main/java/kth/wiljam/fullstackusers/KeyCloak/SecurityConfig.java
+++ src/main/java/kth/wiljam/fullstackusers/KeyCloak/SecurityConfig.java
@@ -1,64 +1,30 @@
-/*
 package kth.wiljam.fullstackusers.KeyCloak;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
-    SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
-        this.keycloakLogoutHandler = keycloakLogoutHandler;
-    }
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
-    @Order(1)
-    @Bean
-    public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers(new AntPathRequestMatcher("/"))
-                .permitAll()
-                .anyRequest()
-                .authenticated();
 
-        http.oauth2Login()
-                .and()
-                .logout()
-                .addLogoutHandler(keycloakLogoutHandler)
-                .logoutSuccessUrl("/");
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(t -> t.disable());
+        http.authorizeHttpRequests(authorize -> {
+            authorize.anyRequest().authenticated();
+        });
+        http.oauth2ResourceServer(t -> {
+            t.jwt(Customizer.withDefaults());
+        });
+        http.sessionManagement(
+                t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
         return http.build();
-    }
-
-    @Order(2)
-    @Bean
-    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers(new AntPathRequestMatcher("/customers*"))
-                .hasRole("USER")
-                .anyRequest()
-                .authenticated();
-        http.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        return http.build();
-    }
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .build();
     }
 }
-*/
